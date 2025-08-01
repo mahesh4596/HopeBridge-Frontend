52f49251b7e09a805acf1679f5f46e4248933b92
mahesh4596 <maheshsingla2007@gmail.com>
2025-08-01 15:03:21 +0530

    add modal in listedmed page

--- src/config/url.js
+++ src/config/url.js
@@ -1,3 +1,3 @@
-export const server_url = "http://localhost:2024";
+// export const server_url = "http://localhost:2024";
 
-// export const server_url = "https://hopebridge-server-4car.onrender.com";
+export const server_url = "https://hopebridge-server-4car.onrender.com";
